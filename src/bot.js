29f57f22831769f44e9be1b9fa02466d5ee934a5
Chus Redondo <j.redondo.pascual@accenture.com>
2021-08-30 05:53:33 +0200

    Polling

--- src/bot.js
+++ src/bot.js
@@ -5,54 +5,77 @@
  */
 
 /**
- * 
+ *
  * @param {function} getOrderBook fetches the order book split by bids and asks
  */
 module.exports = (getOrderBook) => {
-  /**
-   * Start the bot and place initial orders
-   * Watch for changes and fill on interval
-   */
-  async function start() {
-    // get prices
-    const prices = await getOrderBook();
+    const polling = {};
+    /**
+     * Start the bot and place initial orders
+     * Watch for changes and fill on interval
+     */
+    async function start() {
+        // get prices
+        const prices = await getOrderBook();
 
-    // get price ranges
+        // get price ranges
 
-    // create random orders
+        // create random orders
 
-    // start polling
-  }
+        // start polling
+        pollingOrders();
+        pollingBalances();
+    }
 
-  /**
-   * Should return a orders matching
-   * @returns {Order[]} list of all placed orders (bids and asks) no ordering required
-   */
-  async function getPlacedOrders() {
-    throw new Error('Not implemented')
-  }
+    function pollingOrders() {
+        // Interval each 5 seconds to get the prices
+        polling[POLLING.ORDERS] = setInterval(async () => {
+            await fillOrders();
+        }, POLLING.TIME.ORDERS);
+    }
 
-  /**
-   * Should return available balance of the symbol
-   * Total balance - placed orders
-   * @param {String} symbol, e.g. USD or ETH
-   */
-  async function getBalance(symbol) {
-    throw new Error('Not implemented')
-  }
+    function pollingBalances() {
+        // Interval each 30 seconds to get the balances
+        polling[POLLING.BALANCE] = setInterval(async () => {
+            await getBalances();
+        }, POLLING.TIME.BALANCE);
+    }
 
-  /**
-   * trigger filling of orders which are in range
-   * e.g. asks bellow bestAsk and bids above bestBid should be filled
-   */
-  async function fillOrders(symbol) {
-    throw new Error('Not implemented')
-  }
+    /**
+     * Should return a orders matching
+     * @returns {Order[]} list of all placed orders (bids and asks) no ordering required
+     */
+    async function getPlacedOrders() {
+        throw new Error('Not implemented');
+    }
 
-  return {
-    start,
-    getPlacedOrders,
-    getBalance,
-    fillOrders
-  }
-}
+    async function getBalances() {
+        const balancesPromises = Object.values(CURRENCY).map(getBalance);
+        const balances = Promise.all(balancesPromises);
+        return balances;
+    }
+
+    /**
+     * Should return available balance of the symbol
+     * Total balance - placed orders
+     * @param {String} symbol, e.g. USD or ETH
+     */
+    async function getBalance(symbol) {
+        throw new Error('Not implemented');
+    }
+
+    /**
+     * trigger filling of orders which are in range
+     * e.g. asks bellow bestAsk and bids above bestBid should be filled
+     */
+    async function fillOrders(symbol) {
+        throw new Error('Not implemented');
+    }
+
+    return {
+        start,
+        getPlacedOrders,
+        getBalance,
+        fillOrders,
+    };
+};
--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,17 @@ const CURRENCY = Object.freeze({
     USD: 'USD',
 });
 
+const POLLING = Object.freeze({
+    ORDERS: 'ORDERS',
+    BALANCE: 'BALANCE',
+    TIME: {
+        ORDERS: 5000,
+        BALANCE: 30000,
+    },
+});
+
 const constants = Object.freeze({
-    CURRENCY
+    CURRENCY, POLLING
 });
 
 module.exports = constants;
