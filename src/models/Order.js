4c4c41dd795a10f70471d2b57c32d23e3ee687d8
Chus Redondo <j.redondo.pascual@accenture.com>
2021-08-30 07:14:43 +0200

    Order getCost method to calculate balances easily

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,8 +1,19 @@
+const { CURRENCY } = require('../constants');
+
 class Order {
     constructor(price, amount, type) {
         this.price = price;
         this.amount = amount;
         this.type = type;
+    }
+
+    getCost(symbol) {
+        // If ETH increase, USD decrease for bids, and reverse for asks
+        const modEth = this.type === 'bid' ? 1 : -1;
+        const modUsd = modEth * -1;
+        return symbol === CURRENCY.ETH
+            ? this.amount * modEth
+            : this.amount * this.price * modUsd;
     }
 }
 
--- test/models/Order.test.js
+++ test/models/Order.test.js
@@ -1,4 +1,5 @@
 const Order = require('../../src/models/Order');
+const { CURRENCY } = require('../../src/constants');
 
 describe('Order', () => {
     describe('constructor', () => {
@@ -8,6 +9,28 @@ describe('Order', () => {
             const type = 'bid';
             const order = new Order(price, amount, type);
             expect(order).toEqual({ price, amount, type });
+        });
+    });
+    describe('getCost', () => {
+        it('gets eth and usd symbols for a bid order, returns the order amount ETH &  and negative order amount per price USD', ()=>{
+            const price = 200;
+            const amount = 10;
+            const type = 'bid';
+            const order = new Order(price, amount, type);
+            const eth = order.getCost(CURRENCY.ETH);
+            const usd = order.getCost(CURRENCY.USD);
+            expect(eth).toEqual(10);
+            expect(usd).toEqual(-2000)
+        });
+        it('gets eth and usd symbols for a ask order, returns the order negative amount ETH &  and order amount per price USD', ()=>{
+            const price = 200;
+            const amount = 10;
+            const type = 'ask';
+            const order = new Order(price, amount, type);
+            const eth = order.getCost(CURRENCY.ETH);
+            const usd = order.getCost(CURRENCY.USD);
+            expect(eth).toEqual(-10);
+            expect(usd).toEqual(2000)
         });
     });
 });
